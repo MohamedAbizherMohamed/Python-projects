cf96f5118c5891e3fcbe1d2d4d33cea6d18deea2
Mohamed Abizher Mohamed (ID: 02007232) <mabizher1@gmail.com>
2024-05-25 01:44:59 +0000

    simple program

--- basics/bmi.py
+++ basics/bmi.py
@@ -1,12 +1,46 @@
 import sys
 
-# Accept weight ( float ) and height ( float ) as command - line arguments .
-w = float(sys.argv[1])
-h = float(sys.argv[2])
+def fbmi(w, h):
 
-# Set bmi to weight divided by square of height .
-bmi = w / (h * h)
+    # Set bmi to weight divided by square of height .
+    b = w / (h * h)
 
-# Print BMI value.
-print(bmi)
+    weightrange = ["underweight", "normal", "overweight"]
+
+    if b < 18.5:
+        return b, weightrange[0]
+    if 18.5 < b < 24.9:
+        return b, weightrange[1]
+    if b > 24.9:
+        return b, weightrange[2]
+
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Please input 2 float values only")
+        sys.exit(1)
+
+    # Accept weight ( float ) and height ( float ) as command - line arguments .
+    try:
+        w = float(sys.argv[1])
+        h = float(sys.argv[2])
+
+    except ValueError:
+        print("Please input numbers only")
+        sys.exit(1)
+
+    if w < 0 or h < 0:
+        print("Weight/height cannot be negative.")
+        sys.exit(1)
+
+    # Instansiate object and Print BMI.
+    bmi = fbmi(w, h)
+    print(bmi)
+
+if __name__ == "__main__":
+    main()
+
+
+
 
