a44e20c184183effb57898732a9e0f3c1d39d14f
Mohamed Abizher Mohamed (ID: 02007232) <mabizher1@gmail.com>
2024-05-28 13:23:06 +0000

    simple program

--- basics/root.py
+++ basics/root.py
@@ -1,14 +1,16 @@
 import sys
-import math
 
+# Function to calculate the k-th root of c with tolerance e
 def root(k, c, e):
     t = c
     
+    # Iteratively improve t using Newton's method
     while abs(1 - c / t**k) > e:
-        t = t - (t**k - c) / (k * t**k-1 )
+        t = t - (t**k - c) / (k * t**(k-1))
         
     return t
 
+# Main function to handle input and call the root function
 def main():
     if len(sys.argv) != 4:
         print("Please input 3 numbers")
@@ -26,5 +28,7 @@ def main():
     kroot = root(k, c, e)
     print(kroot)
 
+# Entry point of the script
 if __name__ == "__main__":
     main()
+
