06eb9f75afc0f5de5f31b515e957de6b9da6eed4
Mohamed Abizher Mohamed (ID: 02007232) <mabizher1@gmail.com>
2024-05-28 13:54:46 +0000

    simple program

--- basics/gcd.py
+++ basics/gcd.py
@@ -1,2 +1,28 @@
 import sys
 import math
+
+def euclideanalgorithm (p, q):
+
+    while p % q != 0:
+        r = p % q
+        p = q
+        q = r
+        
+    return q
+
+def main():
+    if len(sys.argv) != 3:
+        print("Please only input 2 values")
+
+    try:
+        p = int(sys.argv[1])
+        q = int(sys.argv[2])
+
+    except ValueError:
+        print("Please input valid integers")
+
+    gcd = euclideanalgorithm(p, q)
+    print(gcd)
+
+if __name__ == "__main__":
+    main()
