dc60eca13047392e40c0d824cbd3894a79addc1e
Mohamed Abizher Mohamed (ID: 02007232) <mabizher1@gmail.com>
2024-05-28 13:18:44 +0000

    simple program

--- basics/root.py
+++ basics/root.py
@@ -1,3 +1,30 @@
 import sys
 import math
 
+def root(k, c, e):
+    t = c
+    
+    while abs(1 - c / t**k) > e:
+        t = t - (t**k - c) / (k * t**k-1 )
+        
+    return t
+
+def main():
+    if len(sys.argv) != 4:
+        print("Please input 3 numbers")
+        sys.exit(1)
+    
+    try:
+        k = int(sys.argv[1])
+        c = float(sys.argv[2])
+        e = float(sys.argv[3])
+
+    except ValueError:
+        print("Please input valid numbers")
+        sys.exit(1)
+    
+    kroot = root(k, c, e)
+    print(kroot)
+
+if __name__ == "__main__":
+    main()
